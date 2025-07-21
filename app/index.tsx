1e64345f3d13ee310acff5d037aa615331d070b5
Tom <hernandeztomas584@gmail.com>
2025-07-21 15:14:35 -0300

    counter-app finish

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,45 @@
-import { Text, View } from "react-native";
+import { useCounterStore } from "@/store/counterStore";
+import { Text, TextStyle, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
+  const count = useCounterStore((state) => state.count);
   return (
     <View
       style={{
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
+        gap: 2,
+        flexDirection: "column"
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text style={{fontSize: 80, fontWeight: "bold"}}>{count}</Text>
+     <ActionButtons/>
+    </View>
+    
+  );
+}
+
+
+export const ActionButtons = () =>{
+  const increment = useCounterStore((state) => state.increment);
+  const decrement = useCounterStore((state) => state.decrement);
+
+   const textStyle: TextStyle = {
+    fontSize: 48,
+    fontWeight: 300,
+    color: "#007AFF", 
+    paddingHorizontal: 20,
+  };
+
+   return (
+    <View style={{ flexDirection: "row", justifyContent: "center", alignItems: "center" }}>
+      <TouchableOpacity onPress={decrement}>
+        <Text style={textStyle}>−</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={increment}>
+        <Text style={textStyle}>+</Text>
+      </TouchableOpacity>
     </View>
   );
 }
